87325bd58d2ef60985801c1efb272ff366fe09e2
ChAliraza523 <choudhryaliraza7160@gmail.com>
2021-06-15 14:57:02 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import RoutConfig from "./Rout"
+
+function App() {
+  return (
+    <div>
+      <RoutConfig />
+
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/Rout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from 'react-router-dom';
+
+function RoutConfig() {
+  return (
+    <div>
+        <h1>Hello</h1>
+
+    </div>
+  );
+}
+
+export default RoutConfig;
